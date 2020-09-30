bec064dcea1a6ee263bd24dd67069cc91c916ec0
chips <43027845+Champchips@users.noreply.github.com>
2020-09-30 15:46:49 +0700

    incomplete

--- App.js
+++ App.js
@@ -1,27 +1,23 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
+import Status from './components/Status';
+import MessageList from './components/MessageList'
 export default class App extends React.Component {
   renderMessageList() {
-    return
-    (
-      <View style={styles.content}></View>
-    );
+    return (<View style={styles.content}></View>);
   }
   renderInputMethodEditor() {
-    return
-    (
-      <View style={styles.inputMethodEditor}></View>
-    );
+    return (<View style={styles.inputMethodEditor}></View>);
   }
   renderToolbar() {
-    return
-    (
-      <View style={styles.toolbar}></View>
-    );
+    return (<View style={styles.toolbar}></View>);
   }
   render() {
-    return (<View style={styles.container}> {this.renderMessageList()} {this.renderToolbar()} {this.renderInputMethodEditor()} </View>
-    );
+    return (
+      <View style={styles.container}>
+        <Status />
+        {this.renderMessageList()}{this.renderToolbar()}{this.renderInputMethodEditor()}
+      </View>);
   }
 }
 const styles = StyleSheet.create({
